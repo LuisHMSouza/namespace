58676b61767ae77c907d28401dd6eddcd7e92a93
Luis Henrique Souza <mora0199@algonquinlive.com>
2018-10-25 09:17:01 -0400

    changed function to inside

--- namespace.js
+++ namespace.js
@@ -2,36 +2,29 @@
 Filename: namespace.js
 Author: @Luis Souza
 Description: namespacing exercise
-Date: 2018-10-24
+Date: 2018-10-25
 *********************************/
 
-var MORA0199 = {
+var LUISHMSOUZA = {
 
-    init: function () {
+        init: function () {
 
-        let newDiv = document.createElement("div");
-        newDiv.className = "box";
-        newDiv.textContent = "mora0199";
+            let newDiv = document.createElement("div");
+            newDiv.className = "box";
+            newDiv.textContent = "mora0199";
 
-        let divs = document.getElementById("boxes");
-        divs.appendChild(newDiv);
+            let divs = document.getElementById("boxes");
+            divs.appendChild(newDiv);
 
-        newDiv.addEventListener("click", mouseClick);
-        newDiv.addEventListener("mouseover", mouseOver);
-        newDiv.addEventListener("mouseout", mouseOut);
-    }
-}
-
-function mouseClick() {
-    this.style.borderColor = "#989898";
-    this.style.backgroundColor = "#dba200";
-}
-
-function mouseOver() {
-    this.classList.toggle("highlight");
-}
-
-function mouseOut() {
-    this.classList.toggle("highlight");
-    this.removeAttribute("style");
-}
+            newDiv.addEventListener("click", function () {
+                newDiv.style.borderColor = "#989898";
+                newDiv.style.backgroundColor = "#dba200";
+            });
+            newDiv.addEventListener("mouseover", function () {
+                newDiv.classList.toggle("highlight");
+            });
+            newDiv.addEventListener("mouseout", function () {
+                newDiv.classList.toggle("highlight");
+                newDiv.removeAttribute("style");
+            });
+        };
